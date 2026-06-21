d8aec355676eef6347593381a782cabb9ab59534
9kaluga <9kaluga@gmail.com>
2026-06-21 14:07:32 +0300

    feat(23): изменение цветовой палитры и тесктовок + стили

--- src/components/WeddingCard.tsx
+++ src/components/WeddingCard.tsx
@@ -35,31 +35,54 @@ const WeddingCard: React.FC = () => {
             <p className="address">{weddingData.location.address}</p>
             <div className="divider"></div>
 
-            <h2>Стиль и цветовая палитра нарядов гостей дорогих </h2>
-            <p className="dress-code">{weddingData.dressCode}</p>
+            <h2>Цветовая палитра для нарядов гостей дорогих </h2>
             <div className="color-palette">
               <div className="color-swatches">
+                {/* Ряд 1 */}
                 <div
                   className="color-swatch"
-                  style={{ backgroundColor: "#ddddb7ff" }}
-                  title="Бежевый"
+                  style={{ backgroundColor: "#6a2a3a" }}
+                  data-title="Глубокий бордо"
                 ></div>
                 <div
                   className="color-swatch"
-                  style={{ backgroundColor: "#98623bff" }}
-                  title="Коричневый"
+                  style={{ backgroundColor: "#c62828" }}
+                  data-title="Насыщенный-красный"
                 ></div>
                 <div
                   className="color-swatch"
-                  style={{ backgroundColor: "#ba3c3cff" }}
-                  title="Красный"
+                  style={{ backgroundColor: "#6e4d4dff" }}
+                  data-title="Коричневый"
                 ></div>
                 <div
                   className="color-swatch"
-                  style={{ backgroundColor: "#7a9153ff" }}
-                  title="Зелёный"
+                  style={{ backgroundColor: "#b39183ff" }}
+                  data-title="Бежевый"
+                ></div>
+
+                {/* Ряд 2 */}
+                <div
+                  className="color-swatch"
+                  style={{ backgroundColor: "#8a9a6a" }}
+                  data-title="Оливковый"
+                ></div>
+                <div
+                  className="color-swatch"
+                  style={{ backgroundColor: "#b2b2a0" }}
+                  data-title="Серо-бежевый"
+                ></div>
+                <div
+                  className="color-swatch"
+                  style={{ backgroundColor: "#f0d0c0" }}
+                  data-title="Светло-персиковый"
+                ></div>
+                <div
+                  className="color-swatch"
+                  style={{ backgroundColor: "#f0e0d0" }}
+                  data-title="Кремовый"
                 ></div>
               </div>
+              <p className="dress-code">{weddingData.dressCode}</p>
             </div>
           </div>
         </div>
